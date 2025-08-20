11d7a94d725067d21be02e2388eddbf5d63a3a25
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-19 22:56:41 -0300

    create PetSpecies and update Pet.java

--- src/main/java/com/essj/anima/domain/pet/model/Pet.java
+++ src/main/java/com/essj/anima/domain/pet/model/Pet.java
@@ -1,10 +1,8 @@
 package com.essj.anima.domain.pet.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.essj.anima.domain.user.model.User;
+import jakarta.persistence.*;
 
 @Entity
 public class Pet {
@@ -15,13 +13,29 @@ public class Pet {
     private String name;
     private Integer age;
 
+    @Enumerated(EnumType.STRING)
+    private PetSpecies petSpecies;
+
+    private String breed;
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
     public Pet() {
     }
 
-    public Pet(Long id, String name, Integer age) {
+    public Pet(Long id, String name, Integer age, PetSpecies petSpecies, String breed, String color, User user) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.petSpecies = petSpecies;
+        this.breed = breed;
+        this.color = color;
+        this.user = user;
     }
 
     public Long getId() {
@@ -46,5 +60,37 @@ public class Pet {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public PetSpecies getPetSpecies() {
+        return petSpecies;
+    }
+
+    public void setPetSpecies(PetSpecies petSpecies) {
+        this.petSpecies = petSpecies;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
---
+++ src/main/java/com/essj/anima/domain/pet/model/PetSpecies.java
@@ -0,0 +1,12 @@
+package com.essj.anima.domain.pet.model;
+
+public enum PetSpecies {
+    CACHORRO,
+    GATO,
+    PASSARO,
+    CAVALO,
+    TARTARUGA,
+    PORCO
+
+    // Adicione outros tipos de animais conforme necessário
+}
